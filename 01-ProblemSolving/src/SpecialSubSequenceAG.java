d15eacf31360b4211dfbe45549a25172470f5e6b
Ashvani Verma <ashvani.verma@gmail.com>
2022-04-03 12:10:44 +0530

    SubSequence finding optimal solution by carry forward method

--- 01-ProblemSolving/src/SpecialSubSequenceAG.java
+++ 01-ProblemSolving/src/SpecialSubSequenceAG.java
@@ -1,5 +1,22 @@
 public class SpecialSubSequenceAG {
 
-    public int solve(String A) {
+
+    public static void main(String[] args) {
+        System.out.println(solve("GAB"));
+    }
+
+    public static int solve(String A) {
+        long numberOfACount = 0;
+        long sequenceCount  = 0;
+        for(int i = 0 ;  i < A.length() ;i++) {
+            //update coun when A occurs
+            if(A.charAt(i) == 'A') {
+                numberOfACount++;
+            }else if(A.charAt(i) == 'G') {
+                //when G occurs all the A on left side of this G will make pair so update sequence count
+                sequenceCount += numberOfACount;
+            }
+        }
+        return (int)(sequenceCount % (Math.pow(10,9)+7));
     }
 }
